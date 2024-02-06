f4a5c154aefc296b7879ea901f5c4f60d1366e62
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-02-06 12:24:13 +0100

    Update fetch

--- src/Components/ColRecensione.jsx
+++ src/Components/ColRecensione.jsx
@@ -3,6 +3,12 @@ import SpinnerEB from '../SpinnerEB'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 
+import fantasyBooks from '../books/fantasy.json'
+import historyBooks from '../books/history.json'
+import horrorBooks from '../books/horror.json'
+import romanceBooks from '../books/romance.json'
+import scifiBooks from '../books/scifi.json'
+
 class ColRecensione extends Component {
 
     state = {
@@ -12,6 +18,17 @@ class ColRecensione extends Component {
         loading: true,
         error: false,
         fetchdata: this.fetchData
+    }
+
+    getBookByAsin = (asin) => {
+        let totalBooklist = [
+            ...fantasyBooks,
+            ...historyBooks,
+            ...horrorBooks,
+            ...romanceBooks,
+            ...scifiBooks
+          ]
+          
     }
 
     componentDidUpdate(prevProps) {
@@ -26,7 +43,17 @@ class ColRecensione extends Component {
     }
 
     fetchData = async () => {
-        fetch()
+        fetch(('https://striveschool-api.herokuapp.com/api/comments/'+ this.state.asinEvidenza), {headers: {"Authorization" : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFhMmY2MDE4N2U1YzAwMTgxNGM1ZjYiLCJpYXQiOjE3MDcxODA5NjUsImV4cCI6MTcwODM5MDU2NX0.m18iQGAjFbus5eW2GN5NBb-m4kxJt6NRqXwEYXveaVU"}})
+        .then(response => response.json())
+        .then(data => {
+            this.setState({ error: false })
+            this.setState({ loading: false })
+            console.log(data)
+        })
+        .catch(error => {
+            this.setState({ error: true })
+            this.setState({ loading: false })
+        })
     }
 
     componentDidMount() {
@@ -34,8 +61,6 @@ class ColRecensione extends Component {
     }
 
     render() {
-        console.log(this.state)
-        console.log(this.props)
         let loading = this.state.loading
         let error = this.state.error
         return (
@@ -44,9 +69,9 @@ class ColRecensione extends Component {
       <Card.Img variant="top" src={loading?"https://http.cat/images/102.jpg":error?"https://http.cat/images/400.jpg": this.state.fetchdata.img} />
       <Card.Body>
         <Card.Title>{loading?"Caricamento..":error?"ERRORE" : this.state.fetchdata.nome}</Card.Title>
-        <Card.Text className='text-center'>
+        <div className='text-center'>
         {loading?<SpinnerEB/>:error?'ERRORE': this.state.fetchdata.nome}
-        </Card.Text>
+        </div>
         <Button className='m-1' variant="EpicodeTemaColore1">{!loading && !error?"Aggiungi recensione": ""}</Button>
         <Button className='m-1' variant="EpicodeTemaColore5">{!loading && !error?"Aggiungi al carrello (€)": ""}</Button>
       </Card.Body>
